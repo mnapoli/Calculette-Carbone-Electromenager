84ebe01210f95a95c661936fd5f0004a33424fc7
Matthieu Napoli <matthieu@mnapoli.fr>
2012-01-03 20:46:52 +0100

    Nouveaux pictos

--- public/js/items.js
+++ public/js/items.js
@@ -37,35 +37,35 @@ function Item(id, name, emissionFactor, image, defaultRight) {
 
 var items = new Array();
 
-items.push(new Item("refrig", "Réfrigérateur", 10., "fridge.png"));
-items.push(new Item("refrig-cong", "Réfrigérateur-congélateur", 24., "fridge.png"));
-items.push(new Item("refrig-amer", "Réfrigérateur américain", 65.6, "fridge.png"));
-items.push(new Item("adoucisseur", "Adoucisseur d'eau", 0.8, "fridge.png"));
-items.push(new Item("cafetiere", "Cafetière", 1.86, "fridge.png"));
+items.push(new Item("refrig", "Réfrigérateur", 10., "Refrigerateur.png"));
+items.push(new Item("refrig-cong", "Réfrigérateur-congélateur", 24., "Refrigerateur-cong.png"));
+items.push(new Item("refrig-amer", "Réfrigérateur américain", 65.6, ""));
+items.push(new Item("adoucisseur", "Adoucisseur d'eau", 0.8, ""));
+items.push(new Item("cafetiere", "Cafetière", 1.86, ""));
 items.push(new Item("lave-linge", "Lave-linge", 19.75, "Lave-linge.png"));
 items.push(new Item("lave-vaiss", "Lave-vaisselle", 17.67, "Lave-vaisselle.png"));
 items.push(new Item("seche-linge", "Sèche-linge", 26.66, "Seche-linge.png"));
 
-items.push(new Item("tv-moy", "TV moyenne", 11.9, "fridge.png"));
-items.push(new Item("tv-plasma", "TV Plasma", 31.12, "fridge.png"));
-items.push(new Item("tv-lcd", "TV LCD", 14.14, "fridge.png"));
-items.push(new Item("videoproject", "Vidéo-projecteur", 9.49, "fridge.png"));
-items.push(new Item("box-tv", "Box TV", 5.95, "fridge.png"));
-items.push(new Item("dvd", "Lecteur DVD", 1.3, "fridge.png"));
-items.push(new Item("chaine-hifi", "Chaine Hi-Fi", 4.34, "fridge.png"));
+items.push(new Item("tv-cat", "TV cathodique", 11.9, "TV-cathodique.png"));
+items.push(new Item("tv-plasma", "TV Plasma", 31.12, ""));
+items.push(new Item("tv-lcd", "TV LCD", 14.14, ""));
+items.push(new Item("videoproject", "Vidéo-projecteur", 9.49, ""));
+items.push(new Item("box-tv", "Box TV", 5.95, ""));
+items.push(new Item("dvd", "Lecteur DVD", 1.3, ""));
+items.push(new Item("chaine-hifi", "Chaine Hi-Fi", 4.34, ""));
 
 items.push(new Item("pc-fixe", "PC fixe", 17.24, "Ordi-fixe.png"));
-items.push(new Item("pc-portable", "PC portable", 2.17, "fridge.png"));
-items.push(new Item("box-internet", "Box Internet", 4.15, "fridge.png"));
-items.push(new Item("imprimante", "Imprimante", 1.36, "fridge.png"));
-items.push(new Item("scanner", "Scanner", 1.24, "fridge.png"));
+items.push(new Item("pc-portable", "PC portable", 2.17, "PC-portable.png"));
+items.push(new Item("box-internet", "Box Internet", 4.15, "Box-internet.png"));
+items.push(new Item("imprimante", "Imprimante", 1.36, ""));
+items.push(new Item("scanner", "Scanner", 1.24, ""));
 
-items.push(new Item("aquarium", "Aquarium", 24.4, "fridge.png"));
-items.push(new Item("tel-fixe", "Téléphone fixe", 2.79, "fridge.png"));
+items.push(new Item("aquarium", "Aquarium", 24.4, ""));
+items.push(new Item("tel-fixe", "Téléphone fixe", 2.79, ""));
 items.push(new Item("aspirateur", "Aspirateur", 1.12, "Aspirateur.png"));
-items.push(new Item("eclairage", "Éclairage", 53.94, "fridge.png", true));
-items.push(new Item("fer-repasser", "Fer à repasser", 2.48, "fridge.png"));
-items.push(new Item("pompe-piscine", "Pompe de piscine", 93., "fridge.png"));
+items.push(new Item("eclairage", "Éclairage", 53.94, "", true));
+items.push(new Item("fer-repasser", "Fer à repasser", 2.48, "Fer-repasser.png"));
+items.push(new Item("pompe-piscine", "Pompe de piscine", 93., ""));
 
 /**
  * Find an item
